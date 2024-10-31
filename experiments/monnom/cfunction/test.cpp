529a2cfed682f9666fd7f888a6658f484b46cf4c
Fabian Muehlboeck <fabian.muehlboeck@anu.edu.au>
2024-10-31 11:43:04 +1100

    changes for interface instance call test

--- experiments/monnom/cfunction/test.cpp
+++ experiments/monnom/cfunction/test.cpp
@@ -2,7 +2,13 @@
 
 extern "C" int64_t ctestfun2(int64_t a, int64_t b);
 
-extern "C" int64_t ctestfun(int64_t a, int64_t b){
-	return ctestfun2(a,b);
-}
+extern "C" void* makeBar(int64_t arg);
+extern "C" int64_t getBarA(void* bar);
+extern "C" int64_t compare(void* self, void* targ, void* other);
 
+extern "C" int64_t ctestfun(void* targ, int64_t a, int64_t b){
+	void* bara=makeBar(a);
+	void* barb=makeBar(b);
+	b = getBarA(barb) + getBarA(barb);
+	return ctestfun2(compare(targ, bara, barb),b);
+}
